01449d5b5ff3e62ae4bdaed1acaaff89bc25877f
nareshvjn <50920851+nareshvjn@users.noreply.github.com>
2021-06-15 00:39:21 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,12 +1,13 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 touch "$HOME/.cache/zshhistory"
 #-- Setup Alias in $HOME/zsh/aliasrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-echo "alias u='sudo apt update && sudo apt upgrade -y'
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+
+alias u='sudo apt update && sudo apt upgrade -y'
 alias del='sudo apt autoremove -y'
 alias r='sudo reboot'
 alias o='sudo poweroff'
