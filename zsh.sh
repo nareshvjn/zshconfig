d4aa61928dc0c44db5ac81ded81391593f927bcb
nareshvjn <50920851+nareshvjn@users.noreply.github.com>
2021-06-15 00:39:56 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 touch "$HOME/.cache/zshhistory"
 #-- Setup Alias in $HOME/zsh/aliasrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo "
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/powerlevel10k/powerlevel10k.zsh-theme
 
